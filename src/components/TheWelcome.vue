d5e25bdf29bae0c86075a66b6bd1637e29417eb7
lmp <dev.larpon@gmail.com>
2023-02-20 11:17:37 +0100

    welcome: better instructions

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,6 +5,10 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
 <h1>Welcome!</h1>
 
-<h3>HOW TO PLAY</h3>
+<h2>HOW TO PLAY</h2>
+<p>Eating fruits makes your snake move faster.</p>
+<p>Control the snake with the arrow keys or WASD.</p>
+<p>Don't eat yourself - and do not hit the walls.</p>
 <p>Choose <RouterLink to="/enroll">Enroll</RouterLink> in the menu to start.</p>
+<h3>ENJOY</h3>
 </template>
